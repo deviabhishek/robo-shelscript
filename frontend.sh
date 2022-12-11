2fe65757e6664d7d56f1bcbd39e9353bdc2c36c7
deviabhishek <deviabhishek.devi.com>
2022-12-11 23:13:47 +0530

    f1f

--- frontend.sh
+++ frontend.sh
@@ -12,3 +12,10 @@ ip addr
 sudo systemctl enable nginx
 sudo systemctl start nginx
 sudo systemctl status nginx
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/static/* .
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+systemctl restart nginx
