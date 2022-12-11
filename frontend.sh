833b0b23b4b1764f804a27da494a1569942b5d1c
deviabhishek <deviabhishek.devi.com>
2022-12-11 23:00:50 +0530

    rbfront

---
+++ frontend.sh
@@ -0,0 +1,15 @@
+sudo yum install nginx -y
+sudo amazon-linux-extras install nginx1
+sudo systemctl enable nginx
+sudo systemctl start nginx
+sudo systemctl status nginx
+sudo yum install firewalld
+sudo systemctl enable firewalld
+sudo reboot
+sudo firewall-cmd --zone=public --permanent --add-service=http
+sudo firewall-cmd --zone=public --permanent --add-service=https
+firewall-cmd --reload
+ip addr
+sudo systemctl enable nginx
+sudo systemctl start nginx
+sudo systemctl status nginx
