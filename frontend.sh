4593066dc339bc14cfaabe89a3b9fe7d9bcfda3f
deviabhishek <deviabhishek.devi.com>
2022-12-12 00:32:36 +0530

    s

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,12 @@
+echo nginx start
+
 sudo yum install nginx -y
 sudo amazon-linux-extras install nginx1
 sudo systemctl enable nginx
 sudo systemctl start nginx
 sudo systemctl status nginx
 echo status = $?
+
 sudo yum install firewalld
 sudo systemctl enable firewalld
 sudo firewall-cmd --zone=public --permanent --add-service=http
@@ -11,10 +14,12 @@ sudo firewall-cmd --zone=public --permanent --add-service=https
 firewall-cmd --reload
 ip addr
 echo status = $?
+
 sudo systemctl enable nginx
 sudo systemctl start nginx
 sudo systemctl status nginx
 echo status =$?
+
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html
 rm -rf *
