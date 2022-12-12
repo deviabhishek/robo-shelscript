812d773047957dd0129cf96bfbf3f3e95691b42d
deviabhishek <deviabhishek.devi.com>
2022-12-12 20:38:57 +0530

    mm

--- mongo1.sh
+++ mongo1.sh
@@ -1,5 +1,5 @@
 LOG_FILE=/tmp/mongodb
 echo "creat yum repo for mongoDB"
 
-vi curl -s -o /etc/yum.repos.d/mongodb.repo "https://github.com/deviabhishek/robo-shelscript.git/mongo.repo" &>>$LOG_FILE
+vi curl -s -o /etc/yum.repos.d/mongodb-org-4.2.repo "https://github.com/deviabhishek/mongodb/blob/main/mongo.repo" &>>$LOG_FILE
 echo status = $?
