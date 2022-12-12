e36532ce34bc83b22d8db15f5325bdf08fc59b62
deviabhishek <deviabhishek.devi.com>
2022-12-12 17:04:31 +0530

    mongo

---
+++ mongo.sh
@@ -0,0 +1,18 @@
+LOG_FILE =/tmp/mongodb
+
+echo "setting mongo Db Repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
+echo status - $?
+
+echo "install mongo DB service"
+yum install -y mongodb-org &>>$LOG_FILE
+echo status - $?
+
+echo "update mongo  DB listen add"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf
+echo status - $?
+
+echo "starting mongoDB"
+systemctl enable mongo &>>$LOG_FILE
+systemctl restart mongo &>>$LOG_FILE
+echo status - $?
