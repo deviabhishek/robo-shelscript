dbb477b61341e81f9cf4a553114294a0cb05ba6a
deviabhishek <deviabhishek.devi.com>
2022-12-12 16:23:06 +0530

    spl-var

---
+++ learning-shell/special.var.sh
@@ -0,0 +1,17 @@
+echo "Script Name"
+echo $0
+
+echo "First Argument"
+echo $1
+
+echo "Second Argument"
+echo $2
+
+echo "All Argument"
+echo $*
+
+echo "All Argument"
+echo $@
+
+echo "Num.Of Argument"
+echo $#
