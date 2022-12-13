ae3bd23767c4a23fea389edbeae530e3bac3abf9
deviabhishek <deviabhishek.devi.com>
2022-12-13 12:27:45 +0530

    s

--- mongo1.sh
+++ mongo1.sh
@@ -4,3 +4,5 @@ echo "creat yum repo for mongoDB"
 curl -s -o /etc/yum.repos.d/mongodb-org-4.2.repo "https://github.com/deviabhishek/mongodb/blob/main/mongo.repo" &>>$LOG_FILE
 echo status = $?
 
+yum install -y mongodb-org
+echo status = $?
